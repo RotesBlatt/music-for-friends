d69939b5f6a8e9322d88f9c6bb5fc38e325d28fc
RotesBlatt <RotesBlatt@users.noreply.github.com>
2022-05-11 23:23:45 +0200

    Added a hard reset, in which the index.js file gets relaunched

--- reset.js
+++ reset.js
@@ -1,13 +1,5 @@
 function resetInTerminal(){
     const exec = require('child_process').exec
-
-    exec('killall -9 index', function (error, stdout, stderr) {
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        if (error !== null) {
-            console.log('exec error: ' + error);
-    }
-    })
     
     exec('node index.js',
         function (error, stdout, stderr) {
