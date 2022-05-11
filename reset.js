3d84f0d96bde24e5612f8ba820e1335fca1381a1
RotesBlatt <RotesBlatt@users.noreply.github.com>
2022-05-12 00:22:01 +0200

    Added a hard reset, in which the index.js file gets relaunched

--- reset.js
+++ reset.js
@@ -2,5 +2,8 @@ const spawn = require('child_process').spawn
 process.on('exit', () => {
    const child = spawn('node', ['index.js'])
    child.unref()
+   child.stdout.on('data', function(data) {
+    console.log(data.toString()); 
+    })
 })
 process.exit(0)
