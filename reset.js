97abdf25753ded36268d13928e8b7e0171955377
RotesBlatt <RotesBlatt@users.noreply.github.com>
2022-05-11 23:55:53 +0200

    Added a hard reset, in which the index.js file gets relaunched

--- reset.js
+++ reset.js
@@ -2,7 +2,7 @@ function resetInTerminal(pid){
     const exec = require('child_process').exec
     const process = require('process')
     
-    exec(`bash reset.sh ${pid} ${process.pid}`,
+    exec(`bash reset.sh &`,
         function (error, stdout, stderr) {
             console.log('stdout: ' + stdout);
             console.log('stderr: ' + stderr);
